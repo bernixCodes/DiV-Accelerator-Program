ac85b034ebf84394cbf97a5e2f938d39c5b9cd92
ernies-dev <ernies.tech01@gmail.com>
2023-07-13 08:47:55 +0000

    settings toggle

--- my-grid/index.js
+++ my-grid/index.js
@@ -1,6 +1,11 @@
 const viewPasscodeInput = document.querySelector(".pssWordInput input");
 const viewPasscode = document.querySelector(".eye");
 const unviewPasscode = document.querySelector(".eyeoff");
+const settingIcon = document.querySelector(".setting-icon");
+const optionsDiv = document.querySelector(".options");
+const mainDiv = document.querySelector(".main");
+const profileDiv = document.querySelector(".profile");
+const container = document.querySelector(".container");
 
 viewPasscode.addEventListener("click", () => {
   viewPasscodeInput.type = "text";
@@ -12,4 +17,12 @@ unviewPasscode.addEventListener("click", () => {
   viewPasscodeInput.type = "password";
   unviewPasscode.style.display = "none";
   viewPasscode.style.display = "block";
+});
+
+settingIcon.addEventListener("click", () => {
+  optionsDiv.classList.toggle("hideOptionsDiv");
+  container.classList.toggle("my");
+  mainDiv.classList.toggle("setWidth");
+  //   mainDiv.style.background = "red";
+  //   profileDiv.style.display = "none";
 });
