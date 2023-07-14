d9da238d05c71e6dd7bdb2d810ab890cf0567ddd
ernies-dev <ernies.tech01@gmail.com>
2023-07-14 14:50:42 +0000

    image changer

--- grid_profile/main.js
+++ grid_profile/main.js
@@ -1,8 +1,56 @@
-// alert("javascript is working")
+// ------------------- wait for all element to load -------------------
+$(document).ready(function () {
+  console.log("main javascript file is working");
 
-const uploadButton = document.querySelector("div.img-profile div.iconDiv");
-const uploadInput = document.querySelector(".trick-upload input[type=file]");
+  const uploadButton = document.querySelector("div.img-profile div.iconDiv");
 
-uploadButton.addEventListener("click", function () {
-  uploadInput.click();
+  uploadButton.addEventListener("click", function () {
+    document.querySelector("input[type=file]").click();
+    $("input[type=file]").on("change", function () {
+      var input = this;
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+          $("#image").attr("src", e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    });
+  });
+
+  $(".submit-update").click(function (e) {
+    e.preventDefault();
+
+    // ---------------------span  selecting elements from our webpage----------------------------
+    let student_name = $("span#name");
+    let school = $("span#school_");
+    let profile_email = $("span#email_");
+    let profile_phone = $("span#contact_");
+
+    // $("img").click(function () {
+    //   alert("hhhh");
+    // });
+    // ---------------------- get form data --------------------------
+    let first_name = $("input#firstname").val();
+    let last_name = $("input#lastname").val();
+    let school_name = $("input#school").val();
+    let email = $("input#email").val();
+    let phone = $("input#contact").val();
+
+    student_name.text("");
+    student_name.text(first_name + " " + last_name);
+
+    school.text("");
+    school.text(school_name);
+
+    profile_email.text("");
+    profile_email.text(email);
+
+    profile_phone.text("");
+    profile_phone.text(phone);
+  });
 });
+
+// ------------------- wait for all element to load ends -------------------
